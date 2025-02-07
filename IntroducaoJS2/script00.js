a4ec08afbcb841389ada5a5fc012efe32e370fe3
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-02-07 08:55:14 -0300

    07/02/25

---
+++ IntroducaoJS2/script00.js
@@ -0,0 +1,22 @@
+//tipos de dados
+
+var nome = "bibia"; // string
+var idade = 17; //number
+var nota = 9.5; // number 
+var data; // undefined 
+var aprovado = true; //boolean
+var diploma = null; //nula 
+
+//variáveis constantes (var, let, const)
+
+var nota1 = 5; //declarar
+nota1= 7; //redefinir 
+var nota1 = 10; //redeclarar 
+
+let nota2 = 8; //declarar 
+nota2 = 9; //redefinindo
+//let nota2 = 10; //ERRO não permite redeclarar a nota 
+
+const media = 7.5;//declarar
+// media = 8; // ao iniciar exibe ERRO- não permite redefinir 
+
